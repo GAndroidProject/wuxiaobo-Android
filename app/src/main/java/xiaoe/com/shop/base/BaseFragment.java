5033f848cbbc2b909e1d3c667f47a3d75bd425f2
sunny <sunny@d1851369-bd24-4d56-8f42-d5f936fd0249>
2018-09-26 08:10:15 +0000

    1、基类
    git-svn-id: svn://120.24.217.103/repository/outsource_projects/wu_xb/android/shop/XiaoeShop@237279 d1851369-bd24-4d56-8f42-d5f936fd0249

---
+++ app/src/main/java/xiaoe/com/shop/base/BaseFragment.java
@@ -0,0 +1,112 @@
+package xiaoe.com.shop.base;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import xiaoe.com.common.app.Global;
+import xiaoe.com.network.network_interface.INetworkResponse;
+import xiaoe.com.network.requests.IRequest;
+import xiaoe.com.shop.R;
+
+
+/**
+ * Created by Administrator on 2017/7/17.
+ * <br>des：fragment 基类
+ */
+
+public class BaseFragment extends Fragment implements INetworkResponse {
+    private PopupWindow popupWindow;
+    private TextView mToastText;
+    private boolean isFragmentDestroy = false;
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(popupWindow != null){
+                popupWindow.dismiss();
+            }
+        }
+    };
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        popupWindow = new PopupWindow(getContext());
+        popupWindow.setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
+        popupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+        View pView = LayoutInflater.from(getContext()).inflate(R.layout.layout_custom_toast, null,false);
+        mToastText = (TextView) pView.findViewById(R.id.id_toast_text);
+        popupWindow.setContentView(pView);
+        popupWindow.setBackgroundDrawable(new ColorDrawable(0x00000000));
+        popupWindow.setOutsideTouchable(false);
+        popupWindow.setFocusable(true);
+        isFragmentDestroy = false;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onResponse(final IRequest iRequest, final boolean success, final Object entity) {
+        if(!success && entity instanceof String){
+            if(((String)entity).equals("user login time out")){
+                return;
+            }
+        }
+        if(!isFragmentDestroy){
+            Global.g().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    onMainThreaResponse(iRequest,success,entity);
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        isFragmentDestroy = true;
+        if(popupWindow!= null){
+            popupWindow.dismiss();
+            popupWindow = null;
+        }
+        mHandler.removeCallbacksAndMessages(null);
+        mHandler = null;
+    }
+
+    @Override
+    public void onMainThreaResponse(IRequest iRequest, boolean success, Object entity) {
+
+    }
+    public void ToastCustom(String msg){
+        mHandler.removeMessages(0);
+        mToastText.setText(msg);
+        popupWindow.showAtLocation(getActivity().getWindow().getDecorView(), Gravity.CENTER, 0, 0);
+        mHandler.sendEmptyMessageDelayed(0,1500);
+    }
+    /**
+     * 在弹出对话框时，显示popupWindow，则popupWindow会在对话框下面（被对话框盖住）
+     * 是方法是让popupWindow显示在对话上面
+     * view 是对话框中的view
+     */
+    public void ToastCustomDialog(View view,String msg){
+        mHandler.removeMessages(0);
+        mToastText.setText(msg);
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+        mHandler.sendEmptyMessageDelayed(0,1500);
+    }
+}
---
+++ app/src/main/java/xiaoe/com/shop/base/XiaoeActivity.java
@@ -0,0 +1,135 @@
+package xiaoe.com.shop.base;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import xiaoe.com.common.app.Global;
+import xiaoe.com.network.network_interface.INetworkResponse;
+import xiaoe.com.network.requests.IRequest;
+import xiaoe.com.shop.R;
+
+/**
+ * Created by Administrator on 2017/7/17.
+ */
+
+public class XiaoeActivity extends AppCompatActivity implements INetworkResponse{
+    private static final String TAG = "XiaoeActivity";
+    private final int DISMISS_POPUP_WINDOW = 1;
+    private final int DISMISS_TOAST = 2;
+    private PopupWindow popupWindow;
+    private TextView mToastText;
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(msg.what == DISMISS_POPUP_WINDOW){
+                if(popupWindow != null){
+                    popupWindow.dismiss();
+                }
+            }else if(msg.what == DISMISS_TOAST){
+                if(toast != null){
+                    toast.cancel();
+                }
+            }
+
+        }
+    };
+    private Toast toast;
+    private boolean mHasFocus = false;
+    private boolean isActivityDestroy = false;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        popupWindow = new PopupWindow(this);
+        popupWindow.setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
+        popupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+        View view = LayoutInflater.from(this).inflate(R.layout.layout_custom_toast, null,false);
+        mToastText = (TextView) view.findViewById(R.id.id_toast_text);
+        popupWindow.setContentView(view);
+        popupWindow.setBackgroundDrawable(new ColorDrawable(0x00000000));
+        popupWindow.setOutsideTouchable(false);
+        popupWindow.setFocusable(true);
+        isActivityDestroy = false;
+    }
+
+    public void Toast(String msg){
+        toast = Toast.makeText(this,msg,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    public void onResponse(final IRequest iRequest, final boolean success, final Object entity) {
+        if(!success && entity instanceof String){
+            if(((String)entity).equals("user login time out")){
+            }
+        }
+        if(!isActivityDestroy){
+            Global.g().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    onMainThreaResponse(iRequest,success,entity);
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        mHasFocus = hasFocus;
+    }
+
+    @Override
+    public void onMainThreaResponse(IRequest iRequest, boolean success, Object entity) {
+
+    }
+    public void ToastCustom(String msg){
+        if(mHasFocus){
+            mHandler.removeMessages(DISMISS_POPUP_WINDOW);
+            mToastText.setText(msg);
+            popupWindow.showAtLocation(getWindow().getDecorView(), Gravity.CENTER, 0, 0);
+            mHandler.sendEmptyMessageDelayed(DISMISS_POPUP_WINDOW,1500);
+        }
+    }
+
+    /**
+     * 在弹出对话框时，显示popupWindow，则popupWindow会在对话框下面（被对话框盖住）
+     * 是方法是让popupWindow显示在对话上面
+     * view 是对话框中的view
+     */
+    public void ToastCustomDialog(View view,String msg){
+        mHandler.removeMessages(DISMISS_POPUP_WINDOW);
+        mToastText.setText(msg);
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+        mHandler.sendEmptyMessageDelayed(DISMISS_POPUP_WINDOW,1500);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        isActivityDestroy = true;
+        if(toast != null){
+            toast.cancel();
+        }
+        if(popupWindow != null){
+            popupWindow.dismiss();
+            popupWindow = null;
+        }
+        mHandler.removeCallbacksAndMessages(null);
+        mHandler = null;
+    }
+}
