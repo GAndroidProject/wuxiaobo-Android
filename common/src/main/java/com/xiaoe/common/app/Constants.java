c8f05bff5421d0a10c370a0bcadc487eed28642a
flynnwang <flynnwang@d1851369-bd24-4d56-8f42-d5f936fd0249>
2018-12-03 09:33:00 +0000

    相关平台账号id文件调整
    git-svn-id: svn://120.24.217.103/repository/outsource_projects/wu_xb/android/shop/XiaoeShop@262496 d1851369-bd24-4d56-8f42-d5f936fd0249

--- common/src/main/java/com/xiaoe/common/app/Constants.java
+++ common/src/main/java/com/xiaoe/common/app/Constants.java
@@ -5,8 +5,8 @@ import android.Manifest;
 /**
  * Created by Administrator on 2017/7/17.
  */
-
 public class Constants {
+
     //应用appid 测试环境
     private static final String testAppid = "appiOW1KfWe9943";
     //应用appid 正式环境 (内灰环境【体验店铺（标准版）】)
@@ -15,32 +15,30 @@ public class Constants {
 //    private static final String formalAppid = "appe0MEs6qX8480";
 
     //微信测试环境
-//    private static final String wxLoginAppIdTest = "wx3514f0d221328921";
     private static final String wxAppIdTest = "wx764341f522a6c929";
     //微信开放平台SECRET  测试
-    private static final String WX_SECRET_TEST ="3232dcd861af17be187ca219d535323e";
+    private static final String WX_SECRET_TEST = "3232dcd861af17be187ca219d535323e";
+
     //微信正式环境 (内灰环境【体验店铺（标准版）】)
     private static final String wxAppIdFormal = "wx764341f522a6c929";
+    //微信开放平台SECRET  正式 (内灰环境【体验店铺（标准版）】)
+    private static final String WX_SECRET_FORMAL = "3232dcd861af17be187ca219d535323e";
+
     //微信正式环境 (吴晓波家)
 //    private static final String wxAppIdFormal = "wx0205e0a559628d0b";
-    //微信开放平台SECRET  正式 (内灰环境【体验店铺（标准版）】)
-    private static final String WX_SECRET_FORMAL ="3232dcd861af17be187ca219d535323e";
     //微信开放平台SECRET  正式 (吴晓波家)
 //    private static final String WX_SECRET_FORMAL ="903774ed6c897d4449f29c7e167f9322";
 
-    public static final String AUDIO_PLAY_RESOURCE_ID = "audioPlayResourceid";
-    //上一次购买资源的id
-    public static final String LAST_BUY_RESOURCE_ID = "lastBuyResourceId";
-    //最近一次购买资源的id
-    public static final String RECENTLY_BUY_RESOURCE_ID = "recentlyBuyResourceId";
-    //支付成功
-    public static final String SUCCEED_PAY = "succeedPay";
-    //缓存有效持续时间(秒)
-    public static final int CACHE_DURATION = 432000;//5天
     //Bugly测试环境
     private static final String TEST_BUGLY_ID = "417b763f59";
     //Bugly正式环境
     private static final String FORMAL_BUGLY_ID = "2dcaaeb46a";
+
+    //友盟集成appId  测试
+    private static final String U_MENG_APP_ID_TEST = "5bdac3fbf1f556303b0000ad";
+    //友盟集成appId  正式
+    private static final String U_MENG_APP_ID_FORMAL = "5bfb6135f1f55634ab000419";
+
     //一小时秒数
     public static final int HOUR_SECOND = 3600;
     //一小时毫秒数
@@ -49,17 +47,9 @@ public class Constants {
     public static final int DAY_SECOND = HOUR_SECOND * 24;
     //一天毫秒数
     public static final int DAY_MSEC = DAY_SECOND * 1000;
-    public static final int USE_TYPE = 14;
-    //友盟集成appId  测试
-    private static final String U_MENG_APP_ID_TEST = "5bdac3fbf1f556303b0000ad";
-    //友盟集成appId  正式
-    private static final String U_MENG_APP_ID_FORMAL = "5bfb6135f1f55634ab000419";
+
     //权限常量集
-    public static String[] permissions = {Manifest.permission.READ_PHONE_STATE,Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE};
-    //读写权限
-    public static String[] READ_WRITE_PERMISSIONS = {Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE};
-    public static final String[] mTabNames = {"首页", "已购", "我的"};
+    public static String[] permissions = {Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     // 奖学金和积分相关信息
     public static final String SCHOLARSHIP_ASSET_TYPE = "profit"; // 奖学金
