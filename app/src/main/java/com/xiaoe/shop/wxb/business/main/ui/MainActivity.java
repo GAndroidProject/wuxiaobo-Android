36195a17b68aecb35e042f63c3e29a9604757f47
zak <zak@d1851369-bd24-4d56-8f42-d5f936fd0249>
2018-12-07 15:32:43 +0000

    状态栏修改
    git-svn-id: svn://120.24.217.103/repository/outsource_projects/wu_xb/android/shop/XiaoeShop@265032 d1851369-bd24-4d56-8f42-d5f936fd0249

--- app/src/main/java/com/xiaoe/shop/wxb/business/main/ui/MainActivity.java
+++ app/src/main/java/com/xiaoe/shop/wxb/business/main/ui/MainActivity.java
@@ -119,7 +119,7 @@ public class MainActivity extends XiaoeActivity implements OnBottomTabSelectList
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            StatusBarUtil.setStatusBarColor(getWindow(), Color.parseColor(Global.g().getGlobalColor()), View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            StatusBarUtil.setStatusBarColor(getWindow(), Color.parseColor(Global.g().getGlobalColor()), View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
 
         superVipPresenter = new SuperVipPresenter(this);
